579a1d515bd3e33846cf5ecc09b956dc4b23c266
Vitalii <vitalii.luniev83@gmail.com>
2019-08-15 01:34:05 +0300

    cart

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 //checkbox
 'use strict';
-
 const checkbox = document.querySelectorAll(".filter-check_checkbox");
 checkbox.forEach(function(elem) {
     elem.addEventListener('change', function() {
@@ -12,3 +11,28 @@ checkbox.forEach(function(elem) {
     });
 });
 //cart
+const btnCart = document.getElementById('cart'),
+    modalCart = document.querySelector('.cart'),
+    closeBtn = document.querySelector('.cart-close');
+btnCart.addEventListener('click', function() {
+    modalCart.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+});
+closeBtn.addEventListener('click', function() {
+    modalCart.style.display = 'none';
+    document.body.style.overflow = '';
+});
+//goods
+const cards = document.querySelectorAll('.goods .card'),
+    cartWrapper = document.querySelector('.cart-wrapper'),
+    cartEmpty = document.getElementById('cart-empty');
+
+
+cards.forEach(function(card) {
+    const btn = card.querySelector('button');
+    btn.addEventListener('click', () => {
+        let cardClone = card.cloneNode(true);
+        cartWrapper.appendChild(cardClone);
+        cartEmpty.remove();
+    });
+});
