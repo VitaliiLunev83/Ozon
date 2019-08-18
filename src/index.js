0a55275d26637fea4fc10fb3b8cddae28bba7f39
Vitalii <vitalii.luniev83@gmail.com>
2019-08-18 21:00:40 +0300

    forth commit

--- src/index.js
+++ src/index.js
@@ -83,7 +83,9 @@ function actionPage() {
     const cards = document.querySelectorAll('.goods .card'),
         discountCheckbox = document.getElementById('discount-checkbox'),
         min = document.getElementById('min'),
-        max = document.getElementById('max');
+        max = document.getElementById('max'),
+        search = document.querySelector('.search-wrapper_input'),
+        searchBtn = document.querySelector('.search-btn');
 
     discountCheckbox.addEventListener('click', () => {
         cards.forEach((card) => {
@@ -98,20 +100,41 @@ function actionPage() {
     });
 
     function filterPrice() {
-        cards.forEach((e) => {
+        cards.forEach((card) => {
             const cardPrice = card.querySelector('.card-price'),
-                price = parseFloat(cardPrice.textContent);
+                price = parseFloat(cardPrice.textContent),
+                discount = card.querySelector('.card-sale');
 
-            if (price < min.value || price > max.price) {
-                card.parentNode.remove();
+
+            if ((min.value && price < min.value) || (max.value && price > max.value)) {
+                card.parentNode.style.display = 'none';
+            } else if (discountCheckbox.checked && !discount) {
+                card.parentNode.style.display = 'none';
+            } else {
+                card.parentNode.style.display = '';
             }
-
-
         });
 
     }
     min.addEventListener('change', filterPrice);
     max.addEventListener('change', filterPrice);
+    //search
+    searchBtn.addEventListener('click', () => {
+        const searchText = new RegExp(search.value.trim(), 'i');
+        cards.forEach((card) => {
+            const title = card.querySelector('.card-title');
+            if (!searchText.test(title.textContent)) {
+                card.parentNode.style.display = 'none';
+
+            } else {
+                card.parentNode.style.display = '';
+            }
+        });
+        search.value = '';
+    });
+    // end search
+
+
 }
 //end stockFilter
 toggleChekbox();
